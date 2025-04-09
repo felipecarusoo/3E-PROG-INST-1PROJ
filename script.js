57589fc5ba9d0f47cc7d95126b2ace84bbbec997
Felipe Caruso <101845224+felipecarusoo@users.noreply.github.com>
2025-04-09 09:19:29 -0300

    .

--- script.js
+++ script.js
@@ -19,8 +19,11 @@ const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
 botaoDeAcessibilidade.addEventListener('click', function (){
     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
     opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
-
 })
 
+const alternaContraste = document.getElementById('alterna-contraste')
 
+    alternaContraste.addEventListener('click', function () {
+        document.body.classList.toggle('alto-contraste')
+    })
 })
